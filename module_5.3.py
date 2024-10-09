f38b0bf752282eb258679c8f3eb96049c1274986
Hoelac0ste <Hoelacoste@gmail.com>
2024-10-09 21:05:00 +0300

    Add files via upload

--- module_5.3.py
+++ module_5.3.py
@@ -7,39 +7,74 @@ class House:
         return (f"Название: {self.name}, кол-во этажей: {self.number_of_floors}")
 
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
+        else: print(f"{type(other)} не может использоваться в данной функции")
 
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
+        else: print(f"{type(other)} не может использоваться в данной функции")
+
 
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors <= other
+        else: print(f"{type(other)} не может использоваться в данной функции")
+
 
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors > other
+        else: print(f"{type(other)} не может использоваться в данной функции")
+
 
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors >= other
+        else: print(f"{type(other)} не может использоваться в данной функции")
+
 
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors != other
+        else: print(f"{type(other)} не может использоваться в данной функции")
+
 
     def __add__(self, value):
-        self.number_of_floors += value
+        if isinstance(value, int):
+            self.number_of_floors += value
+        elif isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
+        else:
+            print(f"{type(value)} не может использоваться в данной функции")
         return self
 
     def __radd__(self, value):
-        self.number_of_floors += value
+        self.number_of_floors = self.number_of_floors + value
         return self
 
     def __iadd__(self, value):
-        self.number_of_floors += value
+        self.number_of_floors = self.number_of_floors + value
         return self
 
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h3 = House("Отдохни", 5)
+h4 = "guitar"
 print(h1 == h2)
 print(h1 < h2)
 print(h1 <= h2)
@@ -52,3 +87,4 @@ h3 = h3 + 2
 print(h1)
 print(h2)
 print(h3)
+print(h1 == h4)
